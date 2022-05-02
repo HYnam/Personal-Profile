b1f78683f610e80196da909e99d8edc75b04c073
HYnam <hiuyi.nam@uqconnect.edu.au>
2022-05-02 19:35:29 +0800

    Effect on download CV button

--- app.js
+++ app.js
@@ -35,4 +35,21 @@ function PageTransitions() {
     })
 }
 
+$(function() {  
+    $('.btn-con')
+      .on('mouseenter', function(e) {
+        var parentOffset = $(this).offset(),
+            relX = e.pageX - parentOffset.left,
+            relY = e.pageY - parentOffset.top;
+        $(this).find('span').css({top:relY, left:relX})
+      })
+      .on('mouseout', function(e) {
+        var parentOffset = $(this).offset(),
+            relX = e.pageX - parentOffset.left,
+            relY = e.pageY - parentOffset.top;
+        $(this).find('span').css({top:relY, left:relX})
+      });
+    $('[href=#]').click(function(){return false});
+  });
+
 PageTransitions();
